dfc41b0dcf8372c19b331ae657f0efb381a8968b
Rbasilvas <rbasilva.trabalho@gmail.com>
2026-04-03 19:53:07 -0300

    renomeia projeto para Nexus Finance e adiciona titulo dinamico nos perfis

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,23 @@
-console.log("Frontend carregado com sucesso");
+const campoNome1 = document.getElementById("nome1");
+const campoNome2 = document.getElementById("nome2");
+
+const tituloPessoa1 = document.getElementById("titulo-pessoa1");
+const tituloPessoa2 = document.getElementById("titulo-pessoa2");
+
+function atualizarTitulo(campo, titulo) {
+    const nome = campo.value.trim();
+
+    if (nome === "") {
+        titulo.textContent = "Novo Perfil";
+    } else {
+        titulo.textContent = `Finanças de ${nome}`;
+    }
+}
+
+campoNome1.addEventListener("input", function () {
+    atualizarTitulo(campoNome1, tituloPessoa1);
+});
+
+campoNome2.addEventListener("input", function () {
+    atualizarTitulo(campoNome2, tituloPessoa2);
+});
